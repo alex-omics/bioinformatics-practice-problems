a9b6ce5ef8c2fb50f3d82fd308afc219fdf209de
Alex Arvanitis <alexthearvanitis@gmail.com>
2025-09-09 12:25:53 -0400

    Add hardcoded codon table and path handling
    - Added codon_table_hardcoded.py
    - Path script to codon table & test FASTA file
    - Added __init__.py to transcription for import

---
+++ sequences/translation/codon_table_hardcoded.py
@@ -0,0 +1,29 @@
+"""
+Hardcoded codon-to-amino acid table to demonstrate a manual solution to a common bioinformatics problem. 
+
+Source: https://stackoverflow.com/questions/12547095/how-to-find-the-codons-that-codes-for-the-same-amino-acid-in-python
+"""
+
+codon_table = {
+    'A': ('GCT', 'GCC', 'GCA', 'GCG'),
+    'C': ('TGT', 'TGC'),
+    'D': ('GAT', 'GAC'),
+    'E': ('GAA', 'GAG'),
+    'F': ('TTT', 'TTC'),
+    'G': ('GGT', 'GGC', 'GGA', 'GGG'),
+    'I': ('ATT', 'ATC', 'ATA'),
+    'H': ('CAT', 'CAC'),
+    'K': ('AAA', 'AAG'),
+    'L': ('TTA', 'TTG', 'CTT', 'CTC', 'CTA', 'CTG'),
+    'M': ('ATG',),
+    'N': ('AAT', 'AAC'),
+    'P': ('CCT', 'CCC', 'CCA', 'CCG'),
+    'Q': ('CAA', 'CAG'),
+    'R': ('CGT', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'),
+    'S': ('TCT', 'TCC', 'TCA', 'TCG', 'AGT', 'AGC'),
+    'T': ('ACT', 'ACC', 'ACA', 'ACG'),
+    'V': ('GTT', 'GTC', 'GTA', 'GTG'),
+    'W': ('TGG',),
+    'Y': ('TAT', 'TAC'),
+    '*': ('TAA', 'TAG', 'TGA'),
+}
--- sequences/translation/translation.py
+++ sequences/translation/translation.py
@@ -5,8 +5,35 @@ libraries available in biopython.
 Data Source: https://www.ncbi.nlm.nih.gov/nuccore/D63412.1?report=fasta
 """
 
+import os
+import sys
+from sequences.transcription.transcription_string_replace import transcribe_replace
+
+# Get absolute paths from script to codon table an dinput file
+script_directory = os.path.dirname(__file__)
+codon_table_path = os.path.join(script_directory, "codon_table_hardcoded.py")
+test_file_path = os.path.join(script_directory, "test_file_translation.fasta")
+# print("table path: " + codon_table_path)
+# print("fasta path: " + test_file_path)
 
 
+# Parser function to extract DNA from Nucleotide FASTA files
+def file_parser_lines(file_path):
 
+    fasta_lines = []
+    with open(file_path) as file:
 
+        for line in file:
+            if line.startswith(">"):
+                continue
 
+            fasta_lines.append(line.strip())
+
+    dna = "".join(fasta_lines)
+    return dna
+
+# Test statements
+dna = file_parser_lines(test_file_path)
+print(dna)
+rna = transcribe_replace(dna)
+print(rna)
