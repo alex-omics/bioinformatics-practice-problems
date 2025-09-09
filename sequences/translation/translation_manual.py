3261d71e37cc6a5f7746975c989ccce0b8712f7c
Alex Arvanitis <alexthearvanitis@gmail.com>
2025-09-09 12:30:22 -0400

    Add __init__.py files to all script directories

--- sequences/translation/translation_manual.py
+++ sequences/translation/translation_manual.py
@@ -1,6 +1,5 @@
 """
-Problem: Given an RNA sequence, determine the resulting protein sequence. This method uses the Bio.Data and Bio.Seq 
-libraries available in biopython.
+Problem: Given an RNA sequence, determine the resulting protein sequence. 
 
 Data Source: https://www.ncbi.nlm.nih.gov/nuccore/D63412.1?report=fasta
 """
