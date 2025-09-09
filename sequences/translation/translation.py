f381baa16642a745353705592e694ca8bd227236
Alex Arvanitis <alexthearvanitis@gmail.com>
2025-09-09 11:39:35 -0400

    Create new problem script, add test fasta file

---
+++ sequences/translation/translation.py
@@ -0,0 +1,12 @@
+"""
+Problem: Given an RNA sequence, determine the resulting protein sequence. This method uses the Bio.Data and Bio.Seq 
+libraries available in biopython.
+
+Data Source: https://www.ncbi.nlm.nih.gov/nuccore/D63412.1?report=fasta
+"""
+
+
+
+
+
+
